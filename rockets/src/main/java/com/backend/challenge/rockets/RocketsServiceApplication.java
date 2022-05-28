8fc0a9f6c886899d2c587b0cae417fd836410ea7
Mathias <mathhan01@gmail.com>
2022-05-28 20:23:24 +0200

    spring boot project setup

---
+++ rockets/src/main/java/com/backend/challenge/rockets/RocketsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.backend.challenge.rockets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocketsServiceApplication.class, args);
+	}
+
+}
---
+++ rockets/src/test/java/com/backend/challenge/rockets/RocketsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.challenge.rockets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
